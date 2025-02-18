1a38d33fba28b5fdad5b666b126ef95a89cb2530
Yavuz <yvzerv@gmail.com>
2025-02-18 09:32:13 +0300

    Proje klasörü oluşturuldu.

---
+++ bakkal/src/main/java/org/deneme/bakkal/BakkalApplication.java
@@ -0,0 +1,13 @@
+package org.deneme.bakkal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakkalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakkalApplication.class, args);
+    }
+
+}
---
+++ bakkal/src/test/java/org/deneme/bakkal/BakkalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.deneme.bakkal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakkalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
